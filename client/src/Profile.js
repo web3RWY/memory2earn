677cbe580adb5465680c0af78ca4579e655f7885
Nori.Y <rinsei.rnm@gmail.com>
2022-11-01 00:26:10 +0900

    Profile.js has Hydration Error

---
+++ client/src/Profile.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react'
+import { useConnect, useAccount } from 'wagmi'
+ 
+export function Profile() {
+  const { address, connector, isConnected } = useAccount();  
+  const { connect, connectors, error, isLoading, pendingConnector } = useConnect();
+  const [ _isConnected, _setIsConnected ] = useState(false);
+  const [ _connectors, _setConnectors ] = useState([]);
+
+  useEffect(() => {
+    _setIsConnected(isConnected);
+  }, [isConnected]);
+  
+  useEffect(() => {
+    _setConnectors(connectors);
+  }, [connectors]);
+
+
+//  if(typeof window == "undefined"){
+//     return <div>Loading...</div>
+//  } else {
+     
+     return (
+       <div>
+         {connectors.map((connector) => (
+           <button
+             disabled={!connector.ready}
+             key={connector.id}
+             onClick={() => connect({ connector })}
+           >
+             {connector.name}
+             {!connector.ready && ' (unsupported)'}
+             {isLoading &&
+               connector.id === pendingConnector?.id &&
+               ' (connecting)'}
+           </button>
+         ))}
+    
+         {error && <div>{error.message}</div>}
+       </div>
+     )
+ }
+
+// }
